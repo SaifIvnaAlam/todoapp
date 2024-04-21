ada2adedd73f34eb5785e6b973b9e6f109290d24
saif ivna alam <alam.saifivna@gmail.com>
2024-04-21 17:03:52 +0600

    add: create todo

--- lib/src/features/create_todo/infrastructure/create_todo_facade.dart
+++ lib/src/features/create_todo/infrastructure/create_todo_facade.dart
@@ -1,20 +1,24 @@
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todoapp/src/core/utils/hive_box_names.dart';
 import 'package:todoapp/src/features/home/domain/entities/todo_model.dart';
 import 'package:todoapp/src/features/create_todo/domain/i_create_todo_facade.dart';
 // ignore_for_file: prefer_final_fields, no_leading_underscores_for_local_identifiers
 
 class CreateTodoFacade extends ICreateTodoFacade {
-  var _todoHiveBox = Hive.box<ToDoModel>('todos');
+  var _todoHiveBox = Hive.box<ToDoModel>(todoBox);
 
   @override
   Future<void> createTodo(
       {required String title, required String description}) async {
     ToDoModel _newTodo = ToDoModel(
-        id: const Uuid().v4(),
-        title: title,
-        description: description,
-        isDone: false);
+      id: const Uuid().v4(),
+      title: title,
+      description: description,
+      isDone: false,
+      createdAt: DateFormat('d MMMM y').format(DateTime.now()),
+    );
     await _todoHiveBox.add(_newTodo);
   }
 }
--- lib/src/features/create_todo/presentation/create_todo_page.dart
+++ lib/src/features/create_todo/presentation/create_todo_page.dart
@@ -1,8 +1,12 @@
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoapp/src/core/utils/app_strings.dart';
 import '../../home/application/Todo_cubit/todo_cubit.dart';
+import 'package:todoapp/src/core/presentation/style/text_style.dart';
 import 'package:todoapp/src/core/presentation/componenets/show_error.dart';
+import 'package:todoapp/src/features/create_todo/presentation/components/body_text_field.dart';
+import 'package:todoapp/src/features/create_todo/presentation/components/title_text_field.dart';
 import 'package:todoapp/src/features/create_todo/application/create_todo_cubit/createtodo_cubit.dart';
 
 // ignore_for_file: no_leading_underscores_for_local_identifiers
@@ -21,8 +25,8 @@ class CreateTodoPage extends StatelessWidget {
           MaterialButton(
             onPressed: () {
               _titleController.text.isEmpty
-                  ? showError(
-                      context: context, message: "Title can not be empty")
+                  ? showSnackbar(
+                      context: context, message: forgotTitleErrorText)
                   : context
                       .read<CreatetodoCubit>()
                       .createTodo(
@@ -31,86 +35,35 @@ class CreateTodoPage extends StatelessWidget {
                       .then(
                       (value) {
                         context.read<TodoCubit>().getTodos();
-
                         Navigator.pop(context);
                       },
                     );
             },
             child: const Row(
               children: [
-                Icon(Icons.add),
-                Text("Create"),
+                Text(
+                  "Create",
+                  style: textBtnStyle,
+                ),
               ],
             ),
-          ),
+          )
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
-            child: Text(
-              DateFormat('d MMMM y').format(
-                DateTime.now(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+              child: Text(
+                DateFormat('d MMMM y').format(DateTime.now()),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: TextFormField(
-                controller: _titleController,
-                maxLength: 10000,
-                maxLines: null,
-                onTapOutside: (pointerDownEvent) =>
-                    FocusManager.instance.primaryFocus?.unfocus(),
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-                decoration: const InputDecoration(
-                  counterText: "",
-                  fillColor: Color.fromARGB(255, 201, 199, 199),
-                  filled: true,
-                  border: InputBorder.none,
-                  hintText: 'Title',
-                  hintStyle: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-              ),
-            ),
-          ),
-          TextFormField(
-            controller: _descriptionController,
-            maxLength: 10000,
-            maxLines: null,
-            onTapOutside: (pointerDownEvent) =>
-                FocusManager.instance.primaryFocus?.unfocus(),
-            keyboardType: TextInputType.multiline,
-            style: const TextStyle(
-              fontSize: 20,
-              height: 2,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
-            ),
-            minLines: 4,
-            scrollPhysics: const NeverScrollableScrollPhysics(),
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              counterText: "",
-              hintText: '   Description',
-              hintStyle: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey,
-              ),
-            ),
-          ),
-        ],
+            TitleTextField(titleController: _titleController),
+            BodyTextfield(descriptionController: _descriptionController),
+          ],
+        ),
       ),
     );
   }
