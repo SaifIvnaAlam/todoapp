d68fa86e092243c46b5eab107cfa1d1fb56ca2c2
saif ivna alam <alam.saifivna@gmail.com>
2024-04-21 17:03:12 +0600

    add: app strings better string management

---
+++ lib/src/core/utils/app_strings.dart
@@ -0,0 +1,6 @@
+const String emptyTodoListText = "Hey Champ! Let's start working";
+const String splashText = "Your awesome todo application";
+const String updoCompletedTaskText = "Hey champ! Your Task Wasn't Complete?";
+const String completedTaskText = "Woah! You Have Completed Your Task";
+const String forgotTitleErrorText = "Opps! Did you forgot to add title?";
+const String deletedTaskText = "Hey champ! You have deleted a task";
