0bdc964108a4c1bc0d289ddde56dd5d77d1a1f46
saif ivna alam <alam.saifivna@gmail.com>
2024-04-21 06:23:52 +0600

    add: animated splash page

--- lib/src/core/presentation/app.dart
+++ lib/src/core/presentation/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:todoapp/src/core/utils/app_strings.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:todoapp/src/core/presentation/style/text_style.dart';
 import 'package:todoapp/src/features/home/presentation/home_page.dart';
 import 'package:todoapp/src/features/home/infrastructure/todo_facade.dart';
 import 'package:todoapp/src/features/home/application/Todo_cubit/todo_cubit.dart';
 import 'package:todoapp/src/features/create_todo/infrastructure/create_todo_facade.dart';
+import 'package:todoapp/src/features/update_todo/infrastructure/update_todo_facade.dart';
 import 'package:todoapp/src/features/home/application/complete_todo_cubit/complete_todo_cubit.dart';
 import 'package:todoapp/src/features/create_todo/application/create_todo_cubit/createtodo_cubit.dart';
+import 'package:todoapp/src/features/update_todo/application/update_todo_cubit/update_todo_cubit.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -18,8 +23,10 @@ class App extends StatelessWidget {
         BlocProvider(create: (context) => TodoCubit(TodoFacade())),
         BlocProvider(create: (context) => CreatetodoCubit(CreateTodoFacade())),
         BlocProvider(create: (context) => CompleteTodoCubit()),
+        BlocProvider(create: (context) => UpdateTodoCubit(UpdateTodoFacade())),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           textTheme: GoogleFonts.robotoTextTheme(),
           useMaterial3: true,
@@ -27,7 +34,15 @@ class App extends StatelessWidget {
             seedColor: Colors.cyan.shade400,
           ),
         ),
-        home: const HomePage(),
+        home: AnimatedSplashScreen(
+            duration: 3000,
+            splash: const Text(
+              splashText,
+              style: textBtnStyle,
+            ),
+            nextScreen: const HomePage(),
+            splashTransition: SplashTransition.fadeTransition,
+            backgroundColor: Colors.white),
       ),
     );
   }
