72992a32b5923fcb1cd3b68345ab55f6788dca5a
saif ivna alam <alam.saifivna@gmail.com>
2024-04-21 06:24:10 +0600

    add: space util

--- lib/src/core/presentation/style/app_spacing.dart
+++ lib/src/core/presentation/style/app_spacing.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 const verticalSpace8 = SizedBox(height: 8);
 const verticalSpace16 = SizedBox(height: 16);
+
+//horizontal spacing
+const horizontalSpacing8 = SizedBox(width: 8);
